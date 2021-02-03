c89f727e2c85d6486e17f61e0db4176397feae66
freddymoncayom <57548713+freddymoncayom@users.noreply.github.com>
2021-02-02 23:02:34 -0500

    Speech To Text
    Speech to text

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController, Platform } from '@ionic/angular';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  matches: String[];
+  isRecording = false;
+
+  constructor(public navCtr: NavController, private plt: Platform, private speechRecognition: SpeechRecognition, private cd:ChangeDetectorRef) {
+
+
+  }
+  startListening(){
+    let options = {
+      language: 'es-EC'
+    }
+    this.speechRecognition.startListening().subscribe(matches =>{
+          this.matches = matches;
+          this.cd.detectChanges();
+    });
+    this.isRecording = true;
+  }
+
+  stopListenig(){
+    this.speechRecognition.stopListening().then(()=>{
+      this.isRecording = false;
+    });
+  }
+
+  getPermission(){
+    this.speechRecognition.hasPermission()
+    .then((hasPermission: boolean)=>{
+      if(!hasPermission){
+        this.speechRecognition.requestPermission();
+      }
+    });
+  }
+
+  isIos(){
+    return this.plt.is('ios');
+  }
 
 }
